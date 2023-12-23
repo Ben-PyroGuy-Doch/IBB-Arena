ce8d28bf75c3041ecf819eb426d088d153acf47e
brains93 <grantc0493@gmail.com>
2023-12-23 19:00:25 +0000

    edited logging functions and added Service file configs

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ from time import sleep
 import RPi.GPIO as GPIO
 import logging
 
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='arena.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)-2s %(message)s',  datefmt='%Y-%m-%d %H:%M:%S')
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT) #Flipper pin
 GPIO.setup(13, GPIO.OUT) #Spinner pin
@@ -10,6 +10,7 @@ GPIO.setup(15, GPIO.OUT) #Pit pin
 
 def flipper():
     GPIO.output(11, 1)
+    logging.info('spinner triggered')
     sleep(1)
     GPIO.output(11, 0)
 
@@ -18,16 +19,18 @@ def spinner(state):
         logging.info('spinner turned on')
         GPIO.output(13, 1)
     else:
-        logging.info('spinner turned of')
+        logging.info('spinner turned off')
 
         GPIO.output(13, 0)
 
 def pit():
+    logging.info('pit triggered')
     GPIO.output(15, 1)
     sleep(1)
     GPIO.output(15, 0)
 
 def matchtimer():
+    logging.info('match started')
     flipper()
     sleep(10)
     pit()
@@ -35,3 +38,4 @@ def matchtimer():
     spinner(True)
     sleep(10)
     spinner(False)
+    logging.info('match ended')
