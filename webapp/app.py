d087bcab39b179f2c9c41e9403a665d5d46b9d08
brains93 <grantc0493@gmail.com>
2023-12-26 21:35:25 +0000

    added basic auth

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,25 @@
 from flask import Flask
 from flask import render_template
 from flask_cors import CORS
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 CORS(app)
+auth = HTTPBasicAuth()
 
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+    
 @app.route('/')
+@auth.login_required
 def hello(name=None):
     return render_template('main.html', name=name)
