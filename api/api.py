2406fdd28ebe25f441b3ad88304ec8c928e1d6eb
Ben-PyroGuy-Doch <125480474+Ben-PyroGuy-Doch@users.noreply.github.com>
2024-04-20 20:23:48 +0100

    modified config to UK arena

--- api/api.py
+++ api/api.py
@@ -18,30 +18,82 @@ app.add_middleware(
 async def root():
     return {'message': 'What you doing'}
 
-@app.get('/pit')
-def PIT():
-    pit()
-    return {'message': 'pit triggered'}
+@app.get('/pitup')
+def PITUP():
+    pitup()
+    return {'message': 'Pit up'}
+
+@app.get('/pitdown')
+def PITUP():
+    pitup()
+    return {'message': 'Pit down'}
 
 @app.get('/flipper')
 def FLIPPER():
     flipper()
-    return {'message': 'flipper triggered'}
+    return {'message': 'Flipper triggered'}
 
-@app.get('/spinner/')
-def SPINNER(onof: bool):
-    spinner(onof)
-    if onof:
-        return {'message': 'spinner on'}
-    else:
-        return {'message': 'spinner off'}
+@app.get('/spinner1c')
+def SPINNER1C():
+    spinner1c()
+    return {'message': 'Spinner 1 Clockwise'}
+    
+@app.get('/spinner1a')
+def SPINNER1A():
+    spinner1a()
+    return {'message': 'Spinner 1 Anti-Clockwise'}
 
-@app.get('/start')
-def STARTMATCH():
-    matchtimer()
-    return {'message': 'match started'}
+@app.get('/spinner1off')
+def SPINNER1OFF():
+    spinner1off()
+    return {'message': 'Spinner 1 Off'}
+
+@app.get('/spinner2c')
+def SPINNER2C():
+    spinner2c()
+    return {'message': 'Spinner 2 Clockwise'}
+
+@app.get('/spinner2a')
+def SPINNER2A():
+    spinner2a()
+    return {'message': 'Spinner 2 Anti-Clockwise'}
+    
+@app.get('/spinner2off')
+def SPINNER2OFF():
+    spinner2off()
+    return {'message': 'Spinner 2 Off'}
+
+@app.get('/spinner3c')
+def SPINNER3C():
+    spinner3c()
+    return {'message': 'Spinner 3 Clockwise'}
+
+@app.get('/spinner3a')
+def SPINNER3A():
+    spinner3a()
+    return {'message': 'Spinner 3 Anti-Clockwise'}
+    
+@app.get('/spinner3off')
+def SPINNER3OFF():
+    spinner3off()
+    return {'message': 'Spinner 3 Off'}
+
+@app.get('/start120')
+def STARTMATCH120():
+    matchtimer120()
+    return {'message': '2 min match started'}
+
+@app.get('/start180')
+def STARTMATCH180():
+    matchtimer180()
+    return {'message': '3 min match started'}
+
+@app.get('/startduck')
+def STARTMATCHDUCK():
+    matchtimerDuck()
+    return {'message': 'Duck match started'}
 
 @app.get('/stop')
 def STOPMATCH():
     stopmatch()
-    return {'message': 'match stop flag set'}
+    return {'message': 'Match stop flag set'}
--- api/functions.py
+++ api/functions.py
@@ -5,14 +5,22 @@ import logging
 
 logging.basicConfig(filename='/var/log/arenaapi.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(levelname)-2s %(message)s',  datefmt='%Y-%m-%d %H:%M:%S')
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT) #Flipper pin
-GPIO.setup(13, GPIO.OUT) #Spinner pin
-GPIO.setup(15, GPIO.OUT) #Pit pin
-GPIO.setup(19, GPIO.OUT) #LED mid pin
-GPIO.setup(21, GPIO.OUT) #LED start pin
-GPIO.setup(23, GPIO.OUT) #LED end pin
+GPIO.setup(11, GPIO.OUT)    # Flipper pin
+GPIO.setup(13, GPIO.OUT)    # Spinner 1C pin
+GPIO.setup(16, GPIO.OUT)    # Spinner 1A pin added pns for relays 
+GPIO.setup(18, GPIO.OUT)    # Spinner 2C pin added pns for relays 
+GPIO.setup(22, GPIO.OUT)    # Spinner 2A pin added pns for relays 
+GPIO.setup(24, GPIO.OUT)    # Spinner 3C pin added pns for relays
+GPIO.setup(26, GPIO.OUT)    # Spinner 3A pin added pns for relays
+GPIO.setup(15, GPIO.OUT)    # Pit pin # now a PWM for the Servo 
+p = GPIO.PWM(15, 50)        # Sets up pin 15 at PWM pin 
+p.start(0)                  # Starts running PWM on the pin and sets it to 0
+GPIO.setup(19, GPIO.OUT)    # LED mid pin
+GPIO.setup(21, GPIO.OUT)    # LED start pin
+GPIO.setup(23, GPIO.OUT)    # LED end pin
 
-active_flag = 'active_flag.txt' #file where weapon active flag is stored, 1=weapons active 0=weapons inactive, this is also used to break the match loop
+
+active_flag = 'active_flag.txt' # File where weapon active flag is stored, 1=weapons active 0=weapons inactive, this is also used to break the match loop
 
 def flipper():
     flagcheck()
@@ -21,21 +29,75 @@ def flipper():
     sleep(1)
     GPIO.output(11, 0)
 
-def spinner(state):
-    if state == True:
-        flagcheck()
-        logging.info('spinner turned on')
-        GPIO.output(13, 1)
-    else:
-        logging.info('spinner turned off')
-        GPIO.output(13, 0)
-
-def pit():
+def spinner1c():  
     flagcheck()
-    logging.info('pit triggered')
-    GPIO.output(15, 1)
+    logging.info('Spinner 1 clockwise')
+    GPIO.output(16, 0)
     sleep(1)
-    GPIO.output(15, 0)
+    GPIO.output(13, 1)
+
+def spinner1a():  
+    flagcheck()
+    logging.info('Spinner 1 anti-clockwise')
+    GPIO.output(13, 0)
+    sleep(1)
+    GPIO.output(16, 1)
+
+def spinner1off(): 
+    flagcheck()
+    logging.info('Spinner 1 off')
+    GPIO.output(13, 0)
+    GPIO.output(16, 0)
+
+def spinner2c(): 
+    flagcheck()
+    logging.info('Spinner 2 clockwise')
+    GPIO.output(22, 0)
+    sleep(1)
+    GPIO.output(18, 1)
+
+def spinner2a(): 
+    flagcheck()
+    logging.info('Spinner 2 anti-clockwise')
+    GPIO.output(18, 0)
+    sleep(1)
+    GPIO.output(22, 1)
+
+def spinner2off():  
+    flagcheck()
+    logging.info('Spinner 2 off')
+    GPIO.output(18, 0)
+    GPIO.output(22, 0)
+
+def spinner3c(state): 
+    flagcheck()
+    logging.info('Spinner 3 clockwise')
+    GPIO.output(26, 0)
+    sleep(1)
+    GPIO.output(24, 1)
+
+def spinner3a(state): 
+    flagcheck()
+    logging.info('Spinner 3 anti-clockwise')
+    GPIO.output(24, 0)
+    sleep(1)
+    GPIO.output(26, 1)
+
+def spinner3off(state):  
+    flagcheck()
+    logging.info('Spinner 3 off')
+    GPIO.output(24, 0)
+    GPIO.output(26, 0)
+
+def pitdown():
+    flagcheck()
+    logging.info('pit down')
+    p.ChangeDutyCycle(3)
+
+def pitup():
+    flagcheck()
+    logging.info('pit up')
+    p.ChangeDutyCycle(12)
 
 def lightstart():
     flagcheck()
@@ -73,7 +135,31 @@ def flagcheck():
             return
         
 
-def matchtimer():
+def matchtimer120():
+    logging.info('match started')
+    with open(active_flag, 'w') as file:
+        file.write('1')
+    lightstart()
+    sleep(30) #timer till pit opens and spinner turns on
+    #middle of match, weapons activate
+    lightmid()
+    spinner1c(True)
+    spinner2c(True)
+    spinner3c(True)
+    pitdown()
+    sleep(90) #timer till the end of the match
+    #end of the match
+    lightend()
+    spinner1c(False)
+    spinner1a(False)
+    spinner2c(False)
+    spinner2a(False)
+    spinner3c(False)
+    spinner3a(False)
+    stopmatch()
+    logging.info('match ended')
+
+def matchtimer180():
     logging.info('match started')
     with open(active_flag, 'w') as file:
         file.write('1')
@@ -81,11 +167,42 @@ def matchtimer():
     sleep(60) #timer till pit opens and spinner turns on
     #middle of match, weapons activate
     lightmid()
-    spinner(True)
-    pit()
+    spinner1c(True)
+    spinner2c(True)
+    spinner3c(True)
+    pitdown()
     sleep(120) #timer till the end of the match
     #end of the match
     lightend()
-    spinner(False)
+    spinner1c(False)
+    spinner1a(False)
+    spinner2c(False)
+    spinner2a(False)
+    spinner3c(False)
+    spinner3a(False)
+    stopmatch()
+    logging.info('match ended')
+
+def matchtimerDuck():
+    logging.info('match started')
+    with open(active_flag, 'w') as file:
+        file.write('1')
+    lightstart()
+    sleep(15) #timer till pit opens and spinner turns on
+    #middle of match, weapons activate
+    lightmid()
+    spinner1c(True)
+    spinner2c(True)
+    spinner3c(True)
+    pitdown()
+    sleep(60) #timer till the end of the match
+    #end of the match
+    lightend()
+    spinner1c(False)
+    spinner1a(False)
+    spinner2c(False)
+    spinner2a(False)
+    spinner3c(False)
+    spinner3a(False)
     stopmatch()
     logging.info('match ended')
